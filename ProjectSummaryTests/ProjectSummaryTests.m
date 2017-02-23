fe3c81025ab9beb600b8dbe4cedc603264a9505f
songjiang <2496157421@qq.com>
2017-02-23 09:42:59 +0800

    sum
    shishi

---
+++ ProjectSummaryTests/ProjectSummaryTests.m
@@ -0,0 +1,39 @@
+//
+//  ProjectSummaryTests.m
+//  ProjectSummaryTests
+//
+//  Created by baolicheng on 2017/2/17.
+//  Copyright © 2017年 RenRenFenQi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ProjectSummaryTests : XCTestCase
+
+@end
+
+@implementation ProjectSummaryTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
